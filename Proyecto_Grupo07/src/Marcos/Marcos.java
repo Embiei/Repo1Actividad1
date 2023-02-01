235ac745e8c32db1d49b508b0f2e93744b4d7cea
Marcos <marcos.del.blanco.adan@gmail.com>
2023-02-01 22:48:32 +0100

    Subiendo el paquete marcos

---
+++ Proyecto_Grupo07/src/Marcos/Marcos.java
@@ -0,0 +1,13 @@
+package marcos;
+
+public class Marcos {
+
+	public static void main(String[] args) {
+		nombre();
+	}
+	
+	public static void nombre() {
+		System.out.println("Hola, soy Marcos");
+	}
+	
+}
