79570cb08289e3a1713e7d84e893adcaf90798cb
Himar <himar.rodriguez@gmail.com>
2023-02-02 22:49:26 +0100

    Comida de Marcos

--- Proyecto_Grupo07/src/Marcos/Marcos.java
+++ Proyecto_Grupo07/src/Marcos/Marcos.java
@@ -9,7 +9,7 @@ public class Marcos {
 	public static void nombre() {
 		System.out.println("Hola, soy Marcos");
 		
-		System.out.println("Le gustan las patatas, los espárragos y la col");
+		System.out.println("Le gustan las patatas, los espárragos y el brocolí");
 		
 		System.out.println("Mis hobbies son escalar, pintar y cantar");
 	}
