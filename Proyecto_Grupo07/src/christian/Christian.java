d991e5ad70be8c966cb09bb8e199b53f74111ff8
ubbeck <ubbeck@users.noreply.github.com>
2023-02-02 22:04:22 +0100

    Metodo hobbies

--- Proyecto_Grupo07/src/christian/Christian.java
+++ Proyecto_Grupo07/src/christian/Christian.java
@@ -5,11 +5,19 @@ public class Christian {
 	public static void main(String[] args) {
 		
 		saludar();
+		hobbies();
 
 	}
 	
 	public static void saludar() {
 		System.out.println("Hola, soy Christian");
+	}
+	
+	public static void hobbies() {
+		
+		System.out.println("Billar");
+		System.out.println("Fotografia");
+		System.out.println("Montaña");
 	}
 
 }
